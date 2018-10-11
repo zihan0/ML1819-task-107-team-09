7d71b047b20de7dd1a3539cd34c5e96336b8b861
Nicky <bonellon@tcd.ie>
2018-10-11 16:17:28 +0100

    added plot + cleaned excel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
 
 def LoadDataset(dataset):
     firstLine = True
@@ -11,19 +13,57 @@ def LoadDataset(dataset):
         for row in reader:
             if firstLine:
                 firstLine = False
-                continue;
-
-            dataset.append(row)
+            else:
+                dataset.append(row)
+    dataset.pop(0)
     return dataset;
 
-dataset = [[]]
-dataset = LoadDataset(dataset)
 
-#ToDO:
-# 1. Visualise data
-# 2. Determine which columns to use
-# 3. PREDICT
 
+def plotData(dataset):
+
+    males = [[]]
+    females = [[]]
+
+    for data in dataset:
+        try:
+            if data[1] == 'male':
+                males.append([data[4], data[9]])
+            elif data[1] == 'female':
+                females.append([data[4], data[9]])
+        except:
+            print()
+            #do nothing
+
+
+    males.pop(0)
+    females.pop(0)
+    males = males[0:500]
+
+    fig, ax = plt.subplots()
+
+    x1 = []
+    x2 = []
+    for x in males:
+        x1.append(x[0])
+        x2.append(x[1])
+
+    y1 = []
+    y2 = []
+    females = females[0:500]
+    for y in females:
+        y1.append(y[0])
+        y2.append(y[1])
+
+    ax.scatter(x1, x2, c='blue', marker='o', label='Male')
+    ax.scatter(y1, y2, c='pink', marker='x', label='Female')
+
+    print("1")
+    fig.savefig("graph1.png")
+    print("2")
+
+dataset = LoadDataset([[]])
+plotData(dataset)
 
 
 print("Done")
