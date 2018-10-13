17e15b1dcab1ced067ec724ae86eda9c1c91e3d0
zihan <huangzi@tcd.ie>
2018-10-13 18:24:26 +0100

    Update colorncount.py

--- colorncount.py
+++ colorncount.py
@@ -10,19 +10,16 @@ def colortoflag(X):
     flag = 100
     color = X[:,0]
     uniqcolor = np.unique(color, return_inverse=True) #376 unique color
-    print(len(uniqcolor[0]))
+    #print(len(uniqcolor[0]))
     for i in range (len(color)):
-        for j in range (len(X)):
-            if (np.equal(X[j][0],color[i])): X[j][0] = flag
-
+        temp = np.where(X == color[i])
+        #print (temp)
+        for j in range (len(temp[0])):
+            X[temp[0][j]] = flag
             #X = np.delete(X, (tmp[j]),axis = 1)
-        flag = flag * 4
+        flag = flag * 2
     print(X)
     return X
-
-
-
-
 
 def plotData(X,Y):
     males = [[]]
@@ -32,7 +29,7 @@ def plotData(X,Y):
     i = 0
     for i in range (len(Y)):
         if Y[i] == 'male':
-            males.append([int(str(int(float(X[i][0]))[:4])), X[i][1]])
+            males.append([X[i][0], X[i][1]])
         elif Y[i] == 'female':
             females.append([X[i][0], X[i][1]])
     print(len(males))
@@ -60,9 +57,9 @@ if __name__ == '__main__':
     X = [columns['color'], columns['count'],columns['gender']]
     X = np.asarray(X).T  # change list to array X.shape=(12894, 2)
     X = colortoflag(X)
-    print(X)
+    #print(X)
     Y = X[:,2]
-    print(Y)
+    #print(Y)
     plotData(X,Y)
 
 
